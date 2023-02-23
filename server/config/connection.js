8d3c706a31fa01292a488d7f516683d35bb5f88e
jackieb0707 <jackiebell446@yahoo.com>
2023-02-23 09:57:03 -0600

    added db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/book-collection',
+{
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  dbName: "book-search-db",
 });
 
 module.exports = mongoose.connection;
